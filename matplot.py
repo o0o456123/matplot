1f2b28dfb0f2e93b01cfbcf498e0e093478bb129
wang jian <o0o456123@gmail.com>
2018-02-23 16:48:59 +0800

    test

---
+++ matplot.py
@@ -0,0 +1,45 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+from bs4 import BeautifulSoup
+import requests 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# In[37]:
+
+
+x1 = np.arange(0,360,5)
+y1 = np.sin(x1 * np.pi / 180.0)
+
+x2 = x1
+y2 = np.cos(x2 * np.pi / 180.0)
+
+x3 = x1
+y3 = np.tan(x3 * np.pi / 180.0)
+plt.subplot(231)
+plt.plot(x1,y1,lw=3)
+plt.subplot(232)
+plt.plot(x2,y2,"ro")
+plt.subplot(236)
+plt.plot(x3,y3,"y--",lw=1)
+
+plt.ylabel("y_label")
+plt.xlabel("x_label")
+plt.title("Title")
+
+plt.xlim(-30,390)
+plt.ylim(-5,5)
+
+plt.show()
+
+
+# In[25]:
+
+
+help(plt)
+
